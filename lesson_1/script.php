8624aa7679ce0f8dbd8b9b0803170989b1d0b617
Landesadel <iliillimusicforever@gmail.com>
2022-04-29 17:07:48 +0300

    Update lesson 1
    небольшие правки кода первого урока.

--- lesson_1/script.php
+++ lesson_1/script.php
@@ -1,15 +1,25 @@
 <?php
+
 $name = readline("Введите ваше имя.\n");
 $age = readline("Введите ваш возраст.\n");
-$task1 = readline("$name, какая задача стоит перед вами сегодня?\n");
-$time1 = readline("Сколько по времени займёт данная задача?\n");
-$task2 = readline("$name, какая задача стоит перед вами сегодня?\n");
-$time2 = readline("Сколько по времени займёт данная задача?\n");
-$task3 = readline("$name, какая задача стоит перед вами сегодня?\n");
-$time3 = readline("Сколько по времени займёт данная задача?\n");
-$sum = ($time1 + $time2 + $time3);
-echo ("$name, сегодня у вас запланировано 3 приоритетных задачи на день:\n");
-echo ("- $task1 ($time1),\n");
-echo ("- $task2 ($time2),\n");
-echo ("- $task3 ($time3).\n");
-echo ("Примерное время на выполнение плана = " . $sum . " часа/ов");
+
+$questionTask = "$name, какая задача стоит перед вами сегодня?\n";
+$questionTime = "Сколько по времени займёт данная задача?\n";
+
+$task1 = readline($questionTask);
+$time1 = readline($questionTime);
+
+$task2 = readline($questionTask);
+$time2 = readline($questionTime);
+
+$task3 = readline($questionTask);
+$time3 = readline($questionTime);
+
+$sum = $time1 + $time2 + $time3;
+
+echo "Вас зовут $name, вам $age лет.\n";
+echo "$name, сегодня у вас запланировано 3 приоритетных задачи на день:\n";
+echo "- $task1 ($time1),\n";
+echo "- $task2 ($time2),\n";
+echo "- $task3 ($time3).\n";
+echo "Примерное время на выполнение плана = " . $sum . " часа/ов";
