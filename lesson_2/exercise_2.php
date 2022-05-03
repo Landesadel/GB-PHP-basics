d816c05e8ff76f8e5ba432f4ee0b3ad98978d7fe
Landesadel <iliillimusicforever@gmail.com>
2022-05-03 20:10:56 +0300

    Update exercise_2.php
    доделана 2-я задача. небольшая ошибка в выводе.

--- lesson_2/exercise_2.php
+++ lesson_2/exercise_2.php
@@ -6,9 +6,23 @@ echo "Здравствуйте $userName!\n";
 pointBack:
 $quantityTasks = readline("Введите кол-во задач на сегодня: \n");
 $numb = is_numeric($quantityTasks);
+$taskString = "";
+$n = 0;
+$sum = 0;
 
 if ($numb == true) {
-    echo "это число";
+    do {
+        $questionTask = readline("$userName, напишите какая задача стоит перед вами сегодня?\n");
+        $questionTime = (int)readline("Сколько по времени займёт данная задача?\n");
+        $sum += $questionTime;
+        $timeAndTask = $questionTask . "(" . $questionTime . ")\n";
+        $tasks =$taskString  . "- " . $timeAndTask;
+        $n++;
+    } while ($n < $quantityTasks);
+
+    echo ("$userName, ваш список задач на сегодня: \n" . $tasks);
+    echo ("Примерное время на выполнение плана: " . $sum . " часа/ов\n");
+    echo "Удачного дня, $userName";
 }else {
     echo "Некорректный ввод, введите число!\n";
     goto pointBack;
