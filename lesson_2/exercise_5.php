4dc23c3184e4efa66e19a44568a85bc84f7d118d
Landesadel <iliillimusicforever@gmail.com>
2022-05-06 19:04:19 +0300

    lesson 2
     задание 3 и 5

---
+++ lesson_2/exercise_5.php
@@ -0,0 +1,26 @@
+<?php
+
+$numApple = (int)readline("Введите кол-во яблок: ");
+
+$num = $numApple % 100;
+
+
+if ($num > 19) {
+    $num = $numApple % 10;
+
+}
+
+switch ($num) {
+    case 1: {
+        echo "У вас $numApple яблоко";
+        break;
+    }
+    case 2: case 3: case 4: {
+        echo "У вас $numApple яблока";
+        break;
+    }
+    default: {
+        echo "У вас $numApple яблок";
+        break;
+    }
+}
