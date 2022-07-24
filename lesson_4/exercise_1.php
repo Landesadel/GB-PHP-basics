3df19dc3f724da6d4f58999914974b8947c7de13
Landesadel <iliillimusicforever@gmail.com>
2022-07-24 11:17:53 +0400

    'fixed'

--- lesson_4/exercise_1.php
+++ lesson_4/exercise_1.php
@@ -2,18 +2,11 @@
 
 $numbers = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2];
 
-$definitionNumber = function (int $number): string {
-   /* $result = $number % 2;
-    if ($result == 0) {
-        return (" Число чётное ");
-    } else return (" Число нечетное ");*/
+$definitionNumber = function (int $number): string
+{
 
-    $result = ((bool)($number & 1));
+   return ($number & 1)? "нечётное" : "Чётное";
 
-    if (!$result) return ("Чётное");
-    else return ("нечётное");
 };
 
-$newArr = array_map($definitionNumber, $numbers);
-
-print_r($newArr);
+print_r(array_map($definitionNumber, $numbers));
