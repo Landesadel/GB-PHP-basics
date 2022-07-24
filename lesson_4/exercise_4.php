d9da0316fe209283f04ad471086c4a40c05abf2c
Landesadel <iliillimusicforever@gmail.com>
2022-07-24 11:22:58 +0400

    'fixed_code'

--- lesson_4/exercise_4.php
+++ lesson_4/exercise_4.php
@@ -2,7 +2,7 @@
 
 $factorial = function(int $n) use(&$factorial): int
 {
-    return($n < 2 ? 1 : $n * $factorial($n-1));
+    return $n == 0 ? 1 : $n * $factorial($n-1);
 };
 
 print_r($factorial(5));
